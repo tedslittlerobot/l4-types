a22abc5f71d332aae9614281fe87ef097ff11053
Stef Horner <shorner@wearearchitect.com>
2014-02-10 08:11:41 +0000

    add sample admin fields

---
+++ src/views/update.blade.php
@@ -0,0 +1,37 @@
+@extends('l4-fields::layout')
+
+@section('content')
+
+	{{ Form::open() }}
+
+		<fieldset>
+
+			<legend>Edit {{ ucwords( $type->slug() ) }}:</legend>
+
+			@foreach ( $errors->get('title') as $error )
+				<div class="danger alert">{{ $error }}</div>
+			@foreach
+			<div class="field">
+				{{ Form::text('title', null, ['class' => 'input', 'placeholder' => 'Title']) }}
+			</div>
+
+			@foreach ( $errors->get('slug') as $error )
+				<div class="danger alert">{{ $error }}</div>
+			@foreach
+			<div class="field">
+				{{ Form::text('slug', null, ['class' => 'input', 'placeholder' => 'Slug']) }}
+			</div>
+
+			{{ Form::setModel( $content->content ) }}
+
+			@include( Config::get('types.types.default.' . $type . '.fields') )
+
+			<div class="medium primary btn">
+				{{ Form::submit() }}
+			</div>
+
+		</fieldset>
+
+	{{ Form::close() }}
+
+@stop
